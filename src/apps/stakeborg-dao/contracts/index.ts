509109e9b29e41be8ddfb4ca2886a789ed71a57a
Bogdan Ionita <bogdan.ionita@stream-financial.com>
2022-05-14 11:53:38 +0300

    Add abi for governance-staking and generate contract factories

--- src/apps/stakeborg-dao/contracts/index.ts
+++ src/apps/stakeborg-dao/contracts/index.ts
@@ -3,6 +3,9 @@ import { Injectable, Inject } from '@nestjs/common';
 import { IAppToolkit, APP_TOOLKIT } from '~app-toolkit/app-toolkit.interface';
 import { ContractFactory } from '~contract/contracts';
 import { Network } from '~types/network.interface';
+
+import { GovernanceStaking__factory } from './ethers';
+
 // eslint-disable-next-line
 type ContractOpts = { address: string; network: Network };
 
@@ -11,4 +14,10 @@ export class StakeborgDaoContractFactory extends ContractFactory {
   constructor(@Inject(APP_TOOLKIT) protected readonly appToolkit: IAppToolkit) {
     super((network: Network) => appToolkit.getNetworkProvider(network));
   }
+
+  governanceStaking({ address, network }: ContractOpts) {
+    return GovernanceStaking__factory.connect(address, this.appToolkit.getNetworkProvider(network));
+  }
 }
+
+export type { GovernanceStaking } from './ethers';
